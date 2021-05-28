56df15026e9fab4d20937c3080ec1f28daad7437
Slavic Ouchakov <Slavic.Ouchakov>
2021-05-27 20:35:18 -0400

    Coach route updated

--- controllers/api/coachRoutes.js
+++ controllers/api/coachRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Project, User } = require('../../models');
+const { Home, Booking, Fields } = require('../../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const coachData = await coach.findAll({
       include: [
         {
           model: User,
@@ -15,11 +15,11 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const Coach = coachData.map((Coach) => Coach.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      Coach, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -27,9 +27,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/Coach/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const coachData = await Coach.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -38,10 +38,10 @@ router.get('/project/:id', async (req, res) => {
       ],
     });
 
-    const project = projectData.get({ plain: true });
+    const Coach = coachData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('Coach', {
+      ...Coach,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: Coach }],
     });
 
     const user = userData.get({ plain: true });
@@ -68,7 +68,7 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+/*
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -78,5 +78,5 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
-
+*/
 module.exports = router;
