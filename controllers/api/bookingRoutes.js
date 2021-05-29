2727f22da0c671dba228380b68308124669dfc2b
AliciaJK <alicia.krolak4@gmail.com>
2021-05-29 11:48:32 -0400

    delete route working

--- controllers/api/bookingRoutes.js
+++ controllers/api/bookingRoutes.js
@@ -77,7 +77,7 @@ router.delete('/:id', async (req, res) => {
     const bookingData = await Booking.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        // user_id: req.session.user_id,
       },
     });
 
