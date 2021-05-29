ff33e501651c6137ae9a1885a21c294f49a772ab
AliciaJK <alicia.krolak4@gmail.com>
2021-05-29 11:33:11 -0400

    working on delete booking route

--- controllers/api/bookingRoutes.js
+++ controllers/api/bookingRoutes.js
@@ -2,45 +2,29 @@ const router = require('express').Router();
 const { Coach, Booking, Fields } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// gets all the booking data I think this can be used to build the 
+//GET ALL BOOKINGS---------------
 router.get('/', async (req, res) => {
   try {
     const bookingData = await Booking.findAll();
     res.status(200).json(bookingData);
-    //res.render('homepage');
 
-    // // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // // Pass serialized data and session flag into template
-    // res.render('homepage', { 
-    //   projects, 
-    //   logged_in: req.session.logged_in 
-    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// gets one field by it's id
+// GET BY ID----------------------
 router.get('/:id', async (req, res) => {
   try {
     const bookingData = await Booking.findByPk(req.params.id);
     res.status(200).json(bookingData)
-    // const project = projectData.get({ plain: true });
 
-    // res.render('project', {
-    //   ...project,
-    //   logged_in: req.session.logged_in
-    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// This Functions purpose is to render a users profile I don't know if 
-// we will need this one.
-// Use withAuth middleware to prevent access to route
+//GET USER AUTH -----------------
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
@@ -60,7 +44,7 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-// this function checks if the user is logged in. and if they are not directs them to a log in page
+// GET LOGGED IN USER ----------this function checks if the user is logged in. and if they are not directs them to a log in page
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -69,6 +53,43 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+
+
+//POST A NEW BOOKING--------------- 
+router.post('/', async (req, res) => {
+  try {
+    const newBooking = await Booking.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBooking);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//DELETE A BOOKING -------------------
+router.delete('/:id', async (req, res) => {
+  try {
+    const bookingData = await Booking.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!bookingData) {
+      res.status(404).json({ message: 'No booking found with this id!' });
+      return;
+    }
+
+    res.status(200).json(bookingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
