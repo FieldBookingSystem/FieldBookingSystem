d958b848547e01eb119ab4006141cec90f746a89
Mark Laham <marklaham@gmail.com>
2021-06-01 21:10:59 -0400

    recent changes

--- models/index.js
+++ models/index.js
@@ -2,18 +2,13 @@ const Booking = require('./Booking');
 const Coach = require('./Coach');
 const Fields = require('./Fields');
 
-Coach.belongsToMany(Booking, {
+Coach.hasMany(Booking, {
   foreignKey: 'coach_id',
   onDelete: 'CASCADE'
 });
 
-Fields.belongsToMany(Booking, {
-  through: Fields,
+Booking.belongsTo(Fields, {
   foreignKey: 'field_id'
 });
 
-// Booking.hasOne(Fields, {
-
-//   foreignKey: 'field_id'
-// });
 module.exports = { Booking, Coach, Fields };
