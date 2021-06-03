586e8266a74e9e2bb283bf4a87cbb132359e1e2d
jojo <jodiladouceur.design@gmail.com>
2021-06-03 18:47:15 -0400

    edits to files

--- public/js/booking.js
+++ public/js/booking.js
@@ -3,6 +3,7 @@ console.log("connected");
 $("button").click(function() {
     let internalId = $(this).data('internalid');
     localStorage.setItem("fieldId", JSON.stringify(internalId));
+    document.location.replace('/booking');
 });
 
 
