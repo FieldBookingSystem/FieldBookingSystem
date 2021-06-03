d5378992c9821f71945444af72ab6506b0686770
Slavic Ouchakov <Slavic.Ouchakov>
2021-06-03 18:34:54 -0400

    booking handlebars page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -89,17 +89,17 @@ router.get('/login', (req, res) => {
     return;
   }
 
-  res.render('login');
-});
-//SIGNUP
-router.get('/signup', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('homepage');
-    return;
-  }
+ res.render('login');
+ });
+// //SIGNUP
+// router.get('/signup', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('homepage');
+//     return;
+//   }
 
-  res.render('signup');
-});
+//   res.render('signup');
+// });
 
 module.exports = router;
--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,11 @@ const doc = document.getElementById("searchBtn");
 const cont = document.getElementsByClassName("container2");
 const dateField = document.getElementById("datepicker");
 const loginBtn  = document.getElementById("loginBtn");
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const signupBtn = document.getElementById("signupBtn");
-=======
+
 const homeBtn = document.getElementById("homeBtn");
 const goHome = document.getElementById("goHome")
->>>>>>> bbf1440a858a3ad99e5d0e4b3a6c886f780e9b20
-=======
->>>>>>> 0912c11408d1b35230cb61290df9f79baa465866
 
 //let requestUrl = "http://localhost:5001/api/fields"
 
