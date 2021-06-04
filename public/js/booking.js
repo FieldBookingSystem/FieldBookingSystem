49f2d7a39ddd7c64de6078309ce871694820c828
jojo <jodiladouceur.design@gmail.com>
2021-06-04 19:59:21 -0400

    edits to public files

--- public/js/booking.js
+++ public/js/booking.js
@@ -3,6 +3,7 @@ console.log("connected");
 // let datepicker = document.getElementById("datepicker").nodeValue
 // console.log(datepicker)
 
+// function gets field id
 $("button").click(function() {
     let internalId = $(this).data('internalid');
     localStorage.setItem("fieldId", JSON.stringify(internalId));
@@ -11,6 +12,8 @@ $("button").click(function() {
     // let field = JSON.parse(localStorage.getItem("fieldId"))
     // console.log(field)
 });
+
+
 const bookingFormHandler = async (event) => {
     event.preventDefault();
   
