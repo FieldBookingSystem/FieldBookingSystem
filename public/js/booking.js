2f0ecfcb681d84163b911ff531230c2c9a3aa24e
Slavic Ouchakov <Slavic.Ouchakov>
2021-06-03 21:09:50 -0400

    booking js file and submit

--- public/js/booking.js
+++ public/js/booking.js
@@ -8,6 +8,33 @@ $("button").click(function() {
     // let field = JSON.parse(localStorage.getItem("fieldId"))
     // console.log(field)
 });
+const bookingFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const team = document.querySelector('#team-booking').value.trim();
+    const date = document.querySelector('#date-booking').value.trim();
+    const time = document.querySelector('#timeslot-booking').value.trim();
+    const duration = document.querySelector('#sessionduration-booking').value.trim();
+    const type = document.querySelector('#sessiontype-booking').value.trim();
+  
+    if (team && date && time && duration && type) {
+      const response = await fetch('/api/coach', {
+        method: 'POST',
+        body: JSON.stringify({ team, date, time, duration, type}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace(`booking`);
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.booking-form')
+    .addEventListener('submit', loginFormHandler);
 
 
 
