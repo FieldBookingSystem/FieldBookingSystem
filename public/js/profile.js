2d6dc967bed5c80d262302b28413234958f2a30e
jojo <jodiladouceur.design@gmail.com>
2021-06-04 21:23:41 -0400

    edit to profiles

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,27 @@
-$("button").click(function() {
-  let removeBooking = $(this).data('fieldId');
-  console.log(removeBooking)
-  // let field = JSON.parse(localStorage.getItem("fieldId"))
-  // console.log(field)
-});
+let removeBooking;
+
+// $("button").click(function() {
+//   let remove = $(this).data('fieldid');
+//   removeBooking = remove
+//   console.log(removeBooking);
+
+// });
+ 
+const delBooking = async (event) => {
+  let remove = $(this).data('fieldid');
+  removeBooking = remove
+  console.log(removeBooking);
+
+  const response = await fetch(`/api/booking/${removebooking}`, {
+    method: 'DELETE',
+  });
+  if (response.ok) {
+    console.log("I home this works")
+  } else {
+    console.log("It didn't work")
+  }
+  }
+
+  document
+  .querySelector('.btn')
+  .addEventListener('click', delBooking);
