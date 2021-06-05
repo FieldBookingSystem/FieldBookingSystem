269759c5f8059d659c5fc39326a412b8ee69c121
Mark Laham <marklaham@gmail.com>
2021-06-04 21:15:36 -0400

    added bookinPage.js file to handle booking

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,5 +1,5 @@
 console.log("connected");
-
+//const j = document.getElementById("bookform")
 // let datepicker = document.getElementById("datepicker").nodeValue
 // console.log(datepicker)
 
@@ -11,35 +11,37 @@ $("button").click(function() {
     // let field = JSON.parse(localStorage.getItem("fieldId"))
     // console.log(field)
 });
-const bookingFormHandler = async (event) => {
-    event.preventDefault();
-    let coachId1 = '<%= Session["coachId"] %>';
-    console.log(coachId1);
-    let field = JSON.parse(localStorage.getItem("fieldId"))
-    const team = document.querySelector('#team-booking').value.trim();
-    const date = document.querySelector('#date-booking').value.trim();
-    const time = document.querySelector('#timeslot-booking').value.trim();9
-    // const duration = document.querySelector('#sessionduration-booking').value.trim();
-    // const type = document.querySelector('#sessiontype-booking').value.trim();
+
+// const bookingFormHandler = async (event) => {
+//    // event.preventDefault();
+//     let coachId1 = '<%= Session["coachId"] %>';
+   
+//     console.log(coachId1);
+//     coachId1 = 7;
+//     let field = JSON.parse(localStorage.getItem("fieldId"))
+//     const team = document.querySelector('#team-booking').value.trim();
+//     const date = document.querySelector('#date-booking').value.trim();
+//     const time = document.querySelector('#timeslot-booking').value.trim();9
+//     // const duration = document.querySelector('#sessionduration-booking').value.trim();
+//     // const type = document.querySelector('#sessiontype-booking').value.trim();
   
-    if (team && date && time) {
-      const response = await fetch('/api/booking', {
-        method: 'POST',
-        body: JSON.stringify({field, team, date, time}), //duration and type
-        headers: { 'Content-Type': 'application/json' },
-      });
+//     if (team && date && time) {
+//       const response = await fetch('/api/booking', {
+//         method: 'POST',
+//         body: JSON.stringify({field, team, date, time, coachId1}), //duration and type
+//         headers: { 'Content-Type': 'application/json' },
+//       });
   
-      if (response.ok) {
-        document.location.replace(`/profile/${data.id}`);
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace(`/`);
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   };
   
-  document
-    .querySelector('.booking-form')
-    .addEventListener('submit', bookingFormHandler);
+ 
+//     j.addEventListener('submit', bookingFormHandler);
 
 
 
