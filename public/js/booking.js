858b30c707cd09db308c7fc8766dfba5bb83de71
AliciaJK <alicia.krolak4@gmail.com>
2021-06-04 20:19:50 -0400

    booking js update

--- public/js/booking.js
+++ public/js/booking.js
@@ -6,7 +6,7 @@ console.log("connected");
 $("button").click(function() {
     let internalId = $(this).data('internalid');
     localStorage.setItem("fieldId", JSON.stringify(internalId));
-    let field = JSON.parse(localStorage.getItem("fieldId"))
+    // let field = JSON.parse(localStorage.getItem("fieldId"))
     document.location.replace('/booking');
     // let field = JSON.parse(localStorage.getItem("fieldId"))
     // console.log(field)
